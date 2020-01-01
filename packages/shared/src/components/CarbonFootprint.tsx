fb73d2873bdd500b7906f6252ddf9337e8cd024a
Mike Gajda <github@mikegajda.com>
2020-01-01 13:46:19 -0500

    Add convert-units to make carbon footprint more accessible

--- packages/shared/src/components/CarbonFootprint.tsx
+++ packages/shared/src/components/CarbonFootprint.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
+const convert = require('convert-units')
+
 function convertStringPriceToNumber (input: string): number {
   input = input.replace('$', '')
   let result: number = Number.parseFloat(input)
@@ -13,25 +15,31 @@ function convertStringPriceToNumber (input: string): number {
 function getGramsOfCarbonForPrice (input: number): number {
   return input * 128.9
 }
+
 interface Props {
   price: string
 }
+
 export function CarbonFootprint (props: Props) {
 
+  let price = convertStringPriceToNumber(props.price)
+  let gramsOfCarbon = getGramsOfCarbonForPrice(price)
+  let poundsOfCarbon = convert(gramsOfCarbon).from('g').to('lb')
+  let bestUnit = convert(poundsOfCarbon).from('lb').toBest()
   return (
-      <div className={`row`}>
-        <div className={'col-6 d-flex'}>
-          <h4 className={'align-self-center my-0'}>CO<sub>2</sub> Calculator</h4>
-        </div>
-        <div className={'col-6'}>
-          <div className={'text-right'}>
-            <small className={'font-weight-light'}>CO<sub>2</sub> Footprint</small>
-            <h4>{getGramsOfCarbonForPrice(convertStringPriceToNumber(props.price)).toLocaleString(undefined, {
-              maximumFractionDigits: 2,
-              minimumFractionDigits: 2
-            })}g</h4>
-          </div>
+    <div className={`row`}>
+      <div className={'col-6 d-flex'}>
+        <h4 className={'align-self-center my-0'}>CO<sub>2</sub> Calculator</h4>
+      </div>
+      <div className={'col-6'}>
+        <div className={'text-right'}>
+          <small className={'font-weight-light'}>CO<sub>2</sub> Footprint</small>
+          <h4>{bestUnit.val.toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2
+          })} {bestUnit.plural.toLowerCase()}</h4>
         </div>
       </div>
+    </div>
   )
 }
