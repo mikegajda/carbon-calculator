0678f64240d1a8074fe5f52049980e6c977968ff
Mike Gajda <github@mikegajda.com>
2020-01-01 22:31:11 -0500

    Add social share banner image and update description on the homepage

--- packages/website/src/pages/index.js
+++ packages/website/src/pages/index.js
@@ -1,47 +1,55 @@
-import React from 'react'
+import React from "react";
 import Meta, {
   defaultTitlePrefix,
   getTitleFromHostname
-} from 'components/Meta'
-import Layout from 'components/Layout'
-import { DemoCard } from './demo'
-import { CarbonFootprintCalculationsCard } from './carbon-footprint-calculations'
-import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import BannerImage from 'templates/BannerImage'
-import { AboutUsCards } from './about-us'
-import { ExtensionCard } from './extension'
+} from "components/Meta";
+import Layout from "components/Layout";
+import { DemoCard } from "./demo";
+import { CarbonFootprintCalculationsCard } from "./carbon-footprint-calculations";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
+import BannerImage from "templates/BannerImage";
+import { AboutUsCards } from "./about-us";
+import { ExtensionCard } from "./extension";
 
 class index extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { price: 12.0 }
+  constructor(props) {
+    super(props);
+    this.state = { price: 12.0 };
   }
 
-  render () {
+  render() {
     return (
-      <Layout location={'/'}>
+      <Layout location={"/"}>
         <div className="px-0">
-          <Meta title={`Home`} description={`Description`} url={`/`}/>
+          <Meta
+            title={`Home`}
+            description={`Carbon Calculator detects the price of items as your browse on the web and shows you the estimated
+                carbon impact of these items`}
+            url={`/`}
+            image={`/img/social-share-banner.png`}
+            ogType={"article"}
+            twitterSummaryType={"summary_large_image"}
+          />
           <article className="container p-0 card my-4 shadow">
-            <BannerImage/>
+            <BannerImage />
             <div className="card-body">
-              <p>Carbon Calculator detects the price of items as your browse on the web and shows you the estimated
-                carbon impact of these items.</p>
+              <p>
+                Carbon Calculator detects the price of items as your browse on
+                the web and shows you the estimated carbon impact of these
+                items.
+              </p>
             </div>
           </article>
-          <DemoCard/>
+          <DemoCard />
 
           <ExtensionCard />
-          <CarbonFootprintCalculationsCard/>
+          <CarbonFootprintCalculationsCard />
           <AboutUsCards />
-
-
-
         </div>
       </Layout>
-    )
+    );
   }
 }
 
-export default index
+export default index;
