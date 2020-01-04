4ad9d44b52c2e6fb955dee659d4f60681a4aac95
Mike Gajda <github@mikegajda.com>
2020-01-04 11:29:19 -0500

    Dummy change

--- packages/website/gatsby-node.js
+++ packages/website/gatsby-node.js
@@ -1,11 +1,11 @@
-const each = require("lodash/each");
-const Promise = require("bluebird");
-const path = require("path");
-const Post = path.resolve("./src/templates/Post/index.js");
-const createPaginatedPages = require("gatsby-paginate");
+const each = require('lodash/each')
+const Promise = require('bluebird')
+const path = require('path')
+const Post = path.resolve('./src/templates/Post/index.js')
+const createPaginatedPages = require('gatsby-paginate')
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
+  const { createPage } = actions
 
   return new Promise((resolve, reject) => {
     resolve(
@@ -60,47 +60,47 @@ exports.createPages = ({ graphql, actions }) => {
       )
         .then(({ errors, data }) => {
           if (errors) {
-            console.log(errors);
-            reject(errors);
+            console.log(errors)
+            reject(errors)
           }
-          return data.allFile.edges;
+          return data.allFile.edges
         })
         .then(posts => {
-          posts.sort(function(a, b) {
-            a = new Date(a.node.remark.frontmatter.publishDate);
-            b = new Date(b.node.remark.frontmatter.publishDate);
-            return a > b ? -1 : a < b ? 1 : 0;
-          });
-          return posts;
+          posts.sort(function (a, b) {
+            a = new Date(a.node.remark.frontmatter.publishDate)
+            b = new Date(b.node.remark.frontmatter.publishDate)
+            return a > b ? -1 : a < b ? 1 : 0
+          })
+          return posts
         })
         .then(posts => {
           createPaginatedPages({
             edges: posts.filter(
-              post => post.node.remark.frontmatter.layout === "Post"
+              post => post.node.remark.frontmatter.layout === 'Post'
             ),
             createPage: createPage,
-            pageTemplate: "src/templates/index.js",
+            pageTemplate: 'src/templates/index.js',
             pageLength: 5, // This is optional and defaults to 10 if not used
-            pathPrefix: "posts", // This is optional and defaults to an empty string if not used
+            pathPrefix: 'posts', // This is optional and defaults to an empty string if not used
             context: {} // This is optional and defaults to an empty object if not used
-          });
+          })
 
           posts.forEach(edge => {
-            let node = edge.node;
+            let node = edge.node
 
             if (!node.remark) {
-              return;
+              return
             }
-            const absolutePath = node.absolutePath;
+            const absolutePath = node.absolutePath
             switch (node.remark.frontmatter.layout) {
-              case "Post":
+              case 'Post':
                 return createPage({
                   path: `/posts/${node.name}`,
                   component: Post,
                   context: {
                     absolutePath
                   }
-                });
+                })
               default:
                 return createPage({
                   path: `/posts/${node.name}`,
@@ -108,24 +108,24 @@ exports.createPages = ({ graphql, actions }) => {
                   context: {
                     absolutePath
                   }
-                });
+                })
             }
-          });
+          })
           //return
         })
-    );
-  });
-};
+    )
+  })
+}
 
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       alias: {
-        components: path.resolve(__dirname, "src/components"),
-        templates: path.resolve(__dirname, "src/templates"),
-        scss: path.resolve(__dirname, "src/scss"),
-        config: path.resolve(__dirname, "gatsby-config.js")
+        components: path.resolve(__dirname, 'src/components'),
+        templates: path.resolve(__dirname, 'src/templates'),
+        scss: path.resolve(__dirname, 'src/scss'),
+        config: path.resolve(__dirname, 'gatsby-config.js')
       }
     }
-  });
-};
+  })
+}
